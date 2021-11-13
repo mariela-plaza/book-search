57190fa50b20aaf95d1416172d7d34ab8bfdb60c
Mariela Plaza <mariela.plaza92@gmail.com>
2021-11-13 19:45:05 +0100

    basic components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { BookResultsComponent } from './book/book-results/book-results.component';
+import { BookDetailsComponent } from './book/book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BookResultsComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
