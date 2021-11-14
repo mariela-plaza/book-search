c2b7d4f2d01c2f045e85f443a26fba42db3f60e7
Mariela Plaza <mariela.plaza92@gmail.com>
2021-11-14 14:21:16 +0100

    mend

---
+++ src/app/book/book.model.ts
@@ -0,0 +1,14 @@
+export class Book {
+    constructor(
+        title: string,
+        authors: string[],
+        imagePath: string,
+        avgRating: number,
+        ratingCounts: number,
+        publisher: string,
+        description: string,
+        language: string,
+        categories: string[],
+        pageCount: number
+    ) { }
+}
