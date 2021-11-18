4a6cd71fd77875dc15e95b85a54a8efa4b2b2de2
Mariela Plaza <mariela.plaza92@gmail.com>
2021-11-18 13:26:17 +0100

    Implemented sticky sidebar and Book Details Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     FormsModule,
     RouterModule,
+    BrowserAnimationsModule,
     MatIconModule,
     AppRoutingModule,
   ],
